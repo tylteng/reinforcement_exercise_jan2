cb3ea50439df352e3d6579eef6a3ccfaa201d164
tylteng <teng.ty4@gmail.com>
2018-01-04 15:50:54 -0500

    created working test for to_hash method

--- test_team.rb
+++ test_team.rb
@@ -4,4 +4,13 @@ require './team.rb'
 
 class TestTeam < MiniTest::Test
 
+  def test_to_hash
+    myTeam = Team.new('Asian Glow', 100, ['Tyler', 'Will', 'Coco', 'Lena'])
+
+    hashTeam = myTeam.to_hash
+    hardCode = {team_name: 'Asian Glow', level: 100, points: 0}
+
+    assert_equal(hardCode, hashTeam)
+  end
+
 end
